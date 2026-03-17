97385a6d6ab90295d897b0b62485b0b4817e1a18
Brian Wilson <2091204+brian32768@users.noreply.github.com>
2026-03-17 10:38:41 -0700

    updates for numbat

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Back up the critical files for Home Assistant to the Synology server.
+#  Back up the critical files for Home Assistant and Zigbee2MQTT
 #
 # I wish this was part of the compose file
 
@@ -17,37 +17,34 @@ HOME_ASSISTANT=${PWD}/config
 # Where to write output data
 #OUTPUT_DIR=/net/wenda/volume1/Wildsong/Backups/home-assistant
 OUTPUT_DIR=/tmp/home-assistant
+if [ ! -d $OUTPUT_DIR ]; then
+   mkdir -p $OUTPUT_DIR
+fi
 ls -d $OUTPUT_DIR
-sleep 2
+# Allow Synology to wake up
+#sleep 2
 
 echo Backing up home assistant on $datestamp to $OUTPUT_DIR
 
-# docker buildx build -f Dockerfile.backup sqlite3
+SQLITE3=keinos/sqlite3
 
-for database in home-assistant_v2 zigbee; do
-    echo -n "...working on $database"
-    docker run --rm --net=proxy -v $HOME_ASSISTANT:/config -v $OUTPUT_DIR:/target \
-       sqlite3:latest \
-       sqlite3 ${database}.db ".backup /target/${database}-$datestamp.db"
-    echo
-done
+database=home-assistant_v2
+echo -n "...working on $database... "
+docker run --rm -v $HOME_ASSISTANT:/config --workdir /config \
+       $SQLITE3 \
+       sh -c "sqlite3 ${database}.db .dump" > $OUTPUT_DIR/${database}.${datestamp}.sql
 
 echo Backing up home assistant files to files-$datestamp.tgz
-docker run --rm --net=proxy -v $HOME_ASSISTANT:/config -v $OUTPUT_DIR:/target \
-       sqlite3:latest \
-       tar czf /target/files-$datestamp.tgz --exclude='*.db' .
+cd config && tar czf ${OUTPUT_DIR}/files-${datestamp}.tgz --exclude='*.db' --exclude='.storage/auth' .
+
+echo Backing up Zigbee2MQTT files
+cd ../z2m/z2m_data && tar czf $OUTPUT_DIR/z2m_data-${datestamp}.tgz --exclude log .
 
 # Make things a little more private
-docker run --rm --net=proxy -v $OUTPUT_DIR:/target \
-       sqlite3:latest \
-       chmod 600 /target/files-$datestamp.tgz
-
-# NB doing this with dockers works better because the files are owned by root.
+chmod 600 ${OUTPUT_DIR/*.tgz
 
 echo Deleting $KEEPDAYS day old backup files.
 
-docker run --rm --net=proxy -v $OUTPUT_DIR:/target sqlite3:latest \
-  find /target/ -name '*.db' -mtime +$KEEPDAYS -print -exec rm -f {} \;
+find $OUTPUT_DIR -name '*.db' -mtime +$KEEPDAYS -print -exec rm -f {} \;
+find $OUTPUT_DIR -name '*.tgz' -mtime +$KEEPDAYS -print -exec rm -f {} \;
 
-docker run --rm --net=proxy -v $OUTPUT_DIR:/target sqlite3:latest \
-  find /target/ -name '*.tgz' -mtime +$KEEPDAYS -print -exec rm -f {} \;
